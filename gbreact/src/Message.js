1739cf29865465b363ddab228c2d371cc0a1e7e8
Budalov-Dmitry <northlands91@gmail.com>
2022-09-19 17:29:20 +0300

    Lesson1

---
+++ gbreact/src/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Message.css';
+
+const Message = props => {
+
+    return (
+        <div className="Message">
+            { props.message }
+        </div>
+    );
+};
+
+
+
+export default Message;
