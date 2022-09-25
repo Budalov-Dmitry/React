2bcdd4eca08142ca74d8bbe75a12d56ddad09ed2
Budalov-Dmitry <northlands91@gmail.com>
2022-09-25 21:03:26 +0300

    lesson-2

--- gbreact/src/App.js
+++ gbreact/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
+import Chat from './Chat';
+import React from 'react';
 
 function App() {
 
   const textMessage = 'Мне лень придумывать текст)';
+  
 
   return (
     <div className="App">
       <Message message = {textMessage} />
+      <Chat  />
     </div>
   );
 }
